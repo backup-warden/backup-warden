9ee84f9440ce59aea07ffcb839e98776e7dd6a99
Miguel Marques <miguelkmarques@outlook.com>
2025-06-17 21:30:41 -0400

    Call GetCurrentPackageFullName just once

--- BackupWarden/Utils/RuntimeHelper.cs
+++ BackupWarden/Utils/RuntimeHelper.cs
@@ -3,19 +3,18 @@ using Windows.Win32.Foundation;
 
 namespace BackupWarden.Utils
 {
-    public class RuntimeHelper
+    public static class RuntimeHelper
     {
-        public static unsafe bool IsMSIX
+        private static readonly bool _isMSIX;
+        static RuntimeHelper()
         {
-            get
-            {
-                uint length = 0;
-                // Check if the function call indicates that the process is not packaged (returns ERROR_NOT_FOUND or APPMODEL_ERROR_NO_PACKAGE).
-                // A result of 0 (ERROR_SUCCESS) or ERROR_INSUFFICIENT_BUFFER (122) means it is packaged.
-                // APPMODEL_ERROR_NO_PACKAGE (15700) specifically means the process has no package identity.
-                WIN32_ERROR result = PInvoke.GetCurrentPackageFullName(ref length, null);
-                return result != WIN32_ERROR.APPMODEL_ERROR_NO_PACKAGE;
-            }
+            uint length = 0;
+            // Check if the function call indicates that the process is not packaged (returns ERROR_NOT_FOUND or APPMODEL_ERROR_NO_PACKAGE).
+            // A result of 0 (ERROR_SUCCESS) or ERROR_INSUFFICIENT_BUFFER (122) means it is packaged.
+            // APPMODEL_ERROR_NO_PACKAGE (15700) specifically means the process has no package identity.
+            WIN32_ERROR result = PInvoke.GetCurrentPackageFullName(ref length, null);
+            _isMSIX = result != WIN32_ERROR.APPMODEL_ERROR_NO_PACKAGE;
         }
+        public static bool IsMSIX => _isMSIX;
     }
 }
